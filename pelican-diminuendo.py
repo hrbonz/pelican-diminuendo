8baf2bf61b20fabc35eba6ea48e245cabdbe1352
Stefan 'hr' Berder <hr@bonz.org>
2014-10-04 14:52:16 +0800

    minify html in post processing

--- pelican-diminuendo.py
+++ pelican-diminuendo.py
@@ -1,6 +1,49 @@
 # -*- coding: utf-8 -*-
+from __future__ import with_statement, unicode_literals, print_function
+import logging
+from os import walk
+import os.path
+import six
+
+from pelican import signals
+
+from diminuendo import htmlmin
+
+# make sure open supports unicode py2x/py3x
+if not six.PY3:
+    from codecs import open
+
 
 __author__ = "Stefan \"hr\" Berder"
 __contact__ = "hr@bonz.org"
 __license__ = "BSD 3-Clause"
-__version__ = "0.0.1-alpha"
+__version__ = "0.0.1"
+
+logger = logging.getLogger(__name__)
+
+
+def minify_html(pelican):
+    """Minify all HTML files passed in a pelican instance.
+
+    :param pelican: a pelican object
+    :type pelican: object
+    """
+    for (dirpath, dirnames, filenames) in walk(
+        pelican.settings['OUTPUT_PATH']):
+        for filename in filenames:
+            if filename.endswith('.html'):
+                filepath = os.path.join(dirpath, filename)
+                code = open(filepath, encoding='utf-8').read()
+                with open(filepath, 'w', encoding='utf-8') as fd:
+                    try:
+                        fd.write(htmlmin.minify(code))
+                    except IOError:
+                        logger.error(
+                            "problem minifying ({})".format(IOError))
+                    finally:
+                        fd.close()
+                        
+
+def register():
+    """Register minifying as post processing"""
+    signals.finalized.connect(minify_html)
